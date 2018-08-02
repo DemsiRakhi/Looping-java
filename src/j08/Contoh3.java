8825fd7a70cf22f2e4ce0cab37dee85d0fc61791
RakhiDemsi <RakhiDemsi@LAPTOP-4VLPV026>
2018-08-02 09:47:37 +0700

    Menampilkan 10 bilangan ganjil genap

--- src/j08/Contoh3.java
+++ src/j08/Contoh3.java
@@ -3,12 +3,13 @@ package j08;
 public class Contoh3 {
     public static void main(String[] args) 
     {
+        //Cara Pertama
         //Menampilkan 10 bilangan ganjil pertama
         int x = 1;
         
         for(x = 1; x <= 20; x+=2)
         {
-            System.out.println(x + " ");
+            System.out.print(x + " ");
         }
         
         System.out.println();
@@ -16,10 +17,22 @@ public class Contoh3 {
         //Menampilkan 10 bilangan genap pertama
         for(x = 2; x <= 20; x+=2)
         {
-            System.out.println(x + " ");
+            System.out.print(x + " ");
         }    
         
+        //Cara Kedua
+        for(x = 1; x <20; x++)
+        {
+            if(x % 2 == 1)
+            {
+                System.out.print(x + " ");
+            }
+        for(x = 1; x <=20; x++)
+        {
+            System.out.print(x + " ");   
+        }
     }
+}
 }
     
 
